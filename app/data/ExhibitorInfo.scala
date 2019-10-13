1767f877027580b024a42a79aaefa18a35bb2961
SrTobi <code.databyte@gmail.com>
2019-10-13 13:56:01 +0200

    added new exhibitor data

--- app/data/ExhibitorInfo.scala
+++ app/data/ExhibitorInfo.scala
@@ -3,7 +3,15 @@ package data
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
-case class ExhibitorInfo(id: ExhibitorId, name: String, location: String, icon: String, description: String)
+case class ExhibitorInfo(id: ExhibitorId,
+                         name: String,
+                         location: String,
+                         icon: String,
+                         description: String,
+                         contact: String,
+                         phone: String,
+                         email: String,
+                         filename: String)
 
 object ExhibitorInfo {
   implicit val exhibitorInfoFormat: OFormat[ExhibitorInfo] = (
@@ -11,6 +19,10 @@ object ExhibitorInfo {
       (JsPath \ "name").format[String] and
       (JsPath \ "location").format[String] and
       (JsPath \ "icon").format[String] and
-      (JsPath \ "description").format[String]
+      (JsPath \ "description").format[String] and
+      (JsPath \ "contact").format[String] and
+      (JsPath \ "phone").format[String] and
+      (JsPath \ "email").format[String] and
+      (JsPath \ "filename").format[String]
     )(ExhibitorInfo.apply, unlift(ExhibitorInfo.unapply))
 }
